27403c90259d502902b551bd034ce2006fd47ca7
Julian <jofori@infonaligy.com>
2022-09-19 16:37:19 +0000

    project restructured

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export POSTGRES_USERNAME=oforidatabasedev
+export POSTGRES_PASSWORD=oforidatabasedev
+export POSTGRES_HOST=oforidatabasedev.cmbocrqjd1ez.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=oforidatabasedev
+export AWS_BUCKET=ofori-bucket-dev
 export AWS_REGION=us-east-1
-export AWS_PROFILE=default
+export AWS_PROFILE=microjulian
 export JWT_SECRET=testing
 export URL=http://localhost:8100
